ba9cfe586a24e4ce043de1e899261ed439a8894f
Angel Villahoz-Baleta <avillaho@ccr.buffalo.edu>
2021-08-31 17:20:26 -0400

    Eleventh commit

--- test_reset.sh
+++ test_reset.sh
@@ -6,6 +6,6 @@ make $(cat .env | xargs) vendor-update \
     mariadb-access \
     redfly-database-curator-users \
     redfly-database-development-user
-#cat ./db/migrations/migration_v9.4.0_to_v9.4.1.sql | env $(cat .env) bash -c 'docker exec -i $(docker ps -a | grep "mariadb:10.5" | cut -d" " -f1) mysql $MYSQL_DATABASE -u $MYSQL_USER -p$MYSQL_PASSWORD'
+cat ./db/migrations/migration_v9.4.1_to_v9.5.0.sql | env $(cat .env) bash -c 'docker exec -i $(docker ps -a | grep "mariadb:10.5" | cut -d" " -f1) mysql $MYSQL_DATABASE -u $MYSQL_USER -p$MYSQL_PASSWORD'
 cat ./db/test/author_mshalfon.sql | env $(cat .env) bash -c 'docker exec -i $(docker ps -a | grep "mariadb:10.5" | cut -d" " -f1) mysql $MYSQL_DATABASE -u $MYSQL_USER -p$MYSQL_PASSWORD'
 make $(cat .env | xargs) mariadb-audit-plugin-start
