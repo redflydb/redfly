f6c3cc79197d9370e33baf67c5173ad9ef809477
Xinbo Huang <forhuang8804@gmail.com>
2025-05-14 16:04:46 +0000

    add headers to fix flybase link

--- lib/api/RemoteflybaseHandler.php
+++ lib/api/RemoteflybaseHandler.php
@@ -89,8 +89,18 @@ class RemoteflybaseHandler
         }
         pg_close($dbHandle);
 
-        // Query FBtp From Flybase API 
-        $xml = simplexml_load_string(file_get_contents("https://api.flybase.org/api/v1.0/chadoxml/" . $fbrf));
+        // Query FBtp From Flybase API
+        $context = stream_context_create(
+            array(
+                "http" => array(
+                    "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+                )
+            )
+        );
+
+        $output = file_get_contents("https://api.flybase.org/api/v1.0/chadoxml/" . $fbrf, false, $context);
+        $xml = simplexml_load_string($output);
+
         $results[] = array(
             "fbrf"  => "",
             "fbtp"  => "",
